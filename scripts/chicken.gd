fbe093767f8a10b1c7ee37701f274b3abf66de03
Sakesh Andhavarapu <a.sakesh@gmail.com>
2025-03-15 14:45:36 -0400

    ten chickens jumping

---
+++ scripts/chicken.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+@export var flip_interval: float = 2.0  # Time in seconds between flips
+
+func _ready():
+	$AnimatedSprite2D.play("default")
+	randomize()
+	start_flipping()
+
+func start_flipping():
+	var timer = Timer.new()
+	timer.wait_time = flip_interval
+	timer.autostart = true
+	timer.one_shot = false
+	timer.connect("timeout", _on_flip_timer_timeout)
+	add_child(timer)
+
+func _on_flip_timer_timeout():
+	if randi() % 2 == 0:
+		$AnimatedSprite2D.flip_h = not $AnimatedSprite2D.flip_h  # Randomly flips the sprite horizontally
