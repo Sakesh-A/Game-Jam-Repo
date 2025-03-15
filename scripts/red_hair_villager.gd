7ad4c870d2fe77841577a993f9cc98dc207e2171
Sakesh Andhavarapu <a.sakesh@gmail.com>
2025-03-15 19:33:35 -0400

    last of the npcs

---
+++ scripts/red_hair_villager.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@export var interaction_distance: float = 100.0 # Adjust based on your game
+@export var npc_dialogue: String = "Hello, traveler! I am a villager.\n
+We need to disable player movement when this window appears and offer two buttons to make a decision" # Unique dialogue per NPC
+
+@onready var player = get_node("/root/Game/Player")
+@onready var dialogue_box = get_node("/root/Game/CanvasLayer/DialogueBox") # Get the dialogue box node
+
+func _ready():
+	$InteractionIcon.visible = false # Hide the indicator initially
+
+func _physics_process(delta: float) -> void:
+	$AnimatedSprite2D.play("idle")
+	var direction = global_position.direction_to(player.global_position)
+
+	# Check if the player is within interaction range
+	if global_position.distance_to(player.global_position) < interaction_distance:
+		$InteractionIcon.visible = true # Show indicator
+		$InteractionIcon.play("default")
+		
+		# Check for "interact" input
+		if Input.is_action_just_pressed("interact"):
+			dialogue_box.open_dialogue(npc_dialogue) # Pass unique NPC text
+	else:
+		$InteractionIcon.visible = false # Hide indicator
