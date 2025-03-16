3145efb5c7ca606fa2f0eed6e291275d747c58a3
philipliu <liurphilip@gmail.com>
2025-03-16 15:36:11 -0400

    irain

--- scripts/sky.gd
+++ scripts/sky.gd
@@ -1,22 +1,25 @@
 extends CanvasLayer
 
+@onready var node = get_node("../CanvasLayer/GPUParticles2D")
+
 func _ready():
 	visible = false
 	GameManager.next_day.connect(_next_day)
+	node.visible = false
 	
 func _next_day():
 	$LightRain.visible = false
 	$HeavyRain.visible = false
 	$Dry.visible = false
 	$Drought.visible = false
-	$GPUParticles2D.visible = true
-	if GameManager.weather == -1:
+	node.visible = false
+	if GameManager.weather == 1:
 		$LightRain.visible = true
-		$GPUParticles2D.visible = true
-	elif GameManager.weather == -2:
+		node.visible = true
+	elif GameManager.weather == 2:
 		$HeavyRain.visible = true
-		$GPUParticles2D.visible = true
-	elif GameManager.weather == 1:
+		node.visible = true
+	elif GameManager.weather == -1:
 		$Dry.visible = true
 	else:
 		$Drought.visible = true
